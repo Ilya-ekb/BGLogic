eef0c0a26906068fadb5c3a5a0327ea75897ee6c
Ilya Polovinkin <some@mail.com>
2024-04-18 10:50:15 +0300

    Refactored DIProperties (ROProperty)

---
+++ Core/ROProperty.cs
@@ -0,0 +1,21 @@
+﻿namespace Core
+{
+    public class ROProperty<T> : DiProperty<T>
+    {
+        public T RValue => Value;
+
+        public ROProperty()
+        {
+        }
+
+        public ROProperty(T initValue) : base(initValue)
+        {
+        }
+
+        public void SetValue(T value)
+        {
+            if (!Value.Equals(value))
+                Value = value;
+        }
+    }
+}
--- Core/RwProperty.cs
+++ Core/RwProperty.cs
@@ -1,4 +1,3 @@
-
 namespace Core
 {
     public class RWProperty<T> : DiProperty<T>
@@ -9,21 +8,12 @@ namespace Core
             set => Value = value;
         }
 
-        public RWProperty() : base() {}
-        public RWProperty(T initValue) : base(initValue){}
-    }
-
-    public class ROProperty<T> : DiProperty<T>
-    {
-        public T RValue => Value;
-        
-        public ROProperty() : base() {}
-        public ROProperty(T initValue) : base(initValue){}
-
-        public void SetValue(T value)
+        public RWProperty() : base()
         {
-            Value = value;
         }
 
+        public RWProperty(T initValue) : base(initValue)
+        {
+        }
     }
 }
